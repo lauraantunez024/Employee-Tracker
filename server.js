a4eb656f2088ebe25b01b53c735433fabbd56500
Laura Antunez <lauraantunez024@gmail.com>
2022-06-21 21:38:41 -0400

    started server js file

--- server.js
+++ server.js
@@ -1,1 +1,34 @@
 const inquirer = require("inquirer");
+const mysql = require ("mysql2");
+const cTable = require("console.table");
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "main_db"
+
+});
+
+function start() {
+    Welcome();
+    mainApp();
+}
+
+function welcomeScreen() {
+    console.log("What's up!")
+}
+
+function mainApp() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "choice",
+                message: "What do you want to do?",
+                choices: [
+                    "choice 1",
+                    "choice 2",
+                ]
+            }
+        ])
+}
