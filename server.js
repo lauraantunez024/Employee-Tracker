fcc236a8a545d7dc5a44096cb19aa41ac1d5fa6b
Laura Antunez <lauraantunez024@gmail.com>
2022-06-23 19:21:00 -0400

    added main menu and add employee function

--- server.js
+++ server.js
@@ -11,14 +11,14 @@ const connection = mysql.createConnection({
 
 function start() {
     Welcome();
-    mainApp();
+    base();
 }
 
 function welcomeScreen() {
     console.log("What's up!")
 }
 
-function mainApp() {
+function base() {
     inquirer
         .prompt([
             {
@@ -26,9 +26,94 @@ function mainApp() {
                 name: "choice",
                 message: "What do you want to do?",
                 choices: [
-                    "choice 1",
-                    "choice 2",
+                    "View All Departments",
+                    "View All Roles",
+                    "View All Employees",
+                    "Add Department",
+                    "Add Role",
+                    "Add Employee",
+                    "Update Employee",
+                    "Done",
                 ]
             }
-        ])
+        ]).then((response) => {
+            switch (response.choice) {
+                case "View All Departments":
+                    viewDept();
+                    break;
+                case "View All Roles":
+                    viewRoles();
+                    break;
+                case "View All Employees":
+                    viewEmployees();
+                    break;
+                case "Add Department":
+                    addDept();
+                    break;
+                case "Add Role":
+                    addRole();
+                    break;
+                case "Add Employee":
+                    addEmployees();
+                    break;
+                default:
+                    done();
+
+            }
+        });
+
+}
+
+
+function addEmployee() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "first_name",
+                message: "Enter employee's first name?",
+                validate: (a) => {
+                    if (!a) {
+                        return "Please enter first name"
+                    }
+                    return true;
+                },
+            },
+            {
+                type: "input",
+                name: "last_name",
+                message: "Enter employee's last name",
+                validate: (a) => {
+                    if (!a) {
+                        return "Please enter last name"
+                    }
+                    return true;
+                },
+            },
+        
+            {
+                type: "input",
+                name: "role_id",
+                message: "Enter employee's role id",
+                validate: (a) => {
+                    if (!a) {
+                        return "Please enter role id"
+                    }
+                    return true;
+                },
+            },
+    
+            {
+                type: "input",
+                name: "manager_id",
+                message: "Enter employee's manager id",
+                validate: (a) => {
+                    if (!a) {
+                        return "Please enter first name"
+                    }
+                    return true;
+                },
+            }
+])
+
 }
